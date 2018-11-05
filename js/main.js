72c1ade367fec4a0ad9a202002524c0eac1a534b
Товпинец Кирилл <Tovpinec@mapo.by>
2018-11-05 15:20:59 +0300

    complete footer and singlepage.html'

--- js/main.js
+++ js/main.js
@@ -1,34 +1,4 @@
-let numbers = [];
-const initVal = [];
-let element = document.getElementById('first-number');
-initVal.push(+element.innerText);
-element.innerText = 0;
-numbers.push(element);
 
-element = document.getElementById('second-number');
-initVal.push(+element.innerText);
-element.innerText = 0;
-numbers.push(element);
-
-element = document.getElementById('third-number');
-initVal.push(+element.innerText);
-element.innerText = 0;
-numbers.push(element);
-
-element = document.getElementById('fourth-number');
-initVal.push(+element.innerText);
-element.innerText = 0;
-numbers.push(element);
-
-numbers.forEach((element, index) => {
-    const timer = setInterval(function(){ 
-        let cur = +element.innerText;
-        element.innerText = ++cur; 
-        if(+element.innerText === initVal[index]){
-            clearInterval(timer);
-        }
-    }, 10);
-})
 $(document).ready(function(){
     $('.news-carousel').owlCarousel({
         loop:true,
@@ -51,6 +21,7 @@ $(document).ready(function(){
 
     setTimeout(() => {
         $(".slogan").addClass("fadeInUp");
+        $(".overlay").hide();
     }, 1000);
     $(".overlay").addClass("fadeOut");
     $(".links-carousel").owlCarousel({
@@ -73,4 +44,4 @@ $(document).ready(function(){
     })
   });
 
-
+$(window).scroll(scrollingWindow);
